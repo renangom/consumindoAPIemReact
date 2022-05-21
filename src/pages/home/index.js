e879842fc1d6e205f1b0fbc7398b6b2b16f03465
Renan <renan23eduardo@gmail.com>
2022-05-21 10:49:32 -0300

    Adicionando API-KEY no gitignore

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,7 @@
 import { Container, Movie, MovieList } from "./styles";
 import {useState, useEffect} from 'react'
 import { Link } from "react-router-dom";
+import {key} from '../../api-key';
 
 function Home(){
 
@@ -8,7 +9,7 @@ function Home(){
     const imagePath = 'https://image.tmdb.org/t/p/w500'
     useEffect(() => {
 
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=08de3e09847b38781aa12283a55bdf26&language=en-US&page=1`)
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`)
             .then(response => response.json())
             .then(data => setMovies(data.results))
     }, [])
